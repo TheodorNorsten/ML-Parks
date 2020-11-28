b6ad6d48ae72d87beda25725524b4f9c94235469
TheodorNorsten <62247575+TheodorNorsten@users.noreply.github.com>
2020-11-28 23:27:58 +0100

    Delete Tokens_keys.py

--- Query_and_Autentifiaction/Tokens_keys.py
+++
@@ -1,9 +0,0 @@
-
-# This file stores all the autentification keys and gets imported to the "Twitter_Query_Parks.py"  file.
-# Fill in below autentification-keys variables with your unique ones.
-
-
-Access_token =''
-Access_token_secret =''
-Consumer_apikey =''
-Consumer_secret_apikey =''
